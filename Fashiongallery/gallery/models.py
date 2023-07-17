56a343876e1ff3f1792e240ca40f2edbdb65ce16
andrew <ihesieneandrew3@gmail.com>
2023-07-17 14:12:51 +0100

    andrew

---
+++ Fashiongallery/gallery/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+# class ImageGallery(models.Model):
+#     images = models.FileField()
+ 
+
+class Feature(models.Model):
+    # id: int
+    name= models.CharField(max_length=100)
+    details= models.CharField(max_length=500)
+    # is_true: bool
+
+    def __str__(self):
+        return self.name
+
+
+
+class Service(models.Model):
+    # id: int
+    name= models.CharField(max_length=100)
+    details= models.CharField(max_length=500)
+    # is_true: bool
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
